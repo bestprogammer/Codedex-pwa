f6606782b934b0f5763f68b837530f0aaf9010b5
bestprogammer <enea.rohner@icloud.com>
2025-04-29 15:00:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('codedex-cache-v1').then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
         './offline.html',
-        './styles.css',
         './icon-192.png',
         './icon-512.png'
       ]);
@@ -15,8 +12,8 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then(response => {
-      return response || caches.match('./offline.html');
-    }))
+    fetch(event.request).catch(() => {
+      return caches.match('./offline.html');
+    })
   );
 });
