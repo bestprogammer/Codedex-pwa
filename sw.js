35d79dfd7162cc20f91870478644a002d2dba7c1
bestprogammer <enea.rohner@icloud.com>
2025-05-10 09:18:34 +0200

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(() => {
-    console.log('✅ Service Worker aktiv');
-  }).catch((err) => {
-    console.error('❌ SW Fehler:', err);
-  });
-}
+self.addEventListener('install', (event) => {
+  console.log('📦 Service Worker installiert');
+  event.waitUntil(
+    caches.open('codedex-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
